c2629f27827b5f35dd4239b0d9507d30b67d4e0f
TaYaKi71751 <shantian.tw@gmail.com>
2025-05-23 15:42:43 +0900

    Fix typo

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-			<BrowserRouter base={import.meta.env.BASE_URL}>
+			<BrowserRouter basename={import.meta.env.BASE_URL}>
 			  <Routes>
 			    <Route path="/" element={<></>} />
 			  </Routes>
