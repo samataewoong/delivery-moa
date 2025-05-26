43632d0075613c88776d3866ab4a034df27eda72
TaYaKi71751 <shantian.tw@gmail.com>
2025-05-26 15:37:57 +0900

    Fix page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
 
 function App() {
-  return (
-			<BrowserRouter basename={import.meta.env.BASE_URL}>
-			  <Routes>
-			    <Route path="/" element={<></>} />
-			  </Routes>
-			</BrowserRouter>
-  )
+	return (
+		<BrowserRouter basename={import.meta.env.BASE_URL}>
+			<Header />
+			<Routes>
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+ return (
+    <div className="header" style={{
+        backgroundColor: 'skyblue',
+        width: '100vw',
+        height: '200px'
+    }}>
+
+    </div>
+ )
+}
