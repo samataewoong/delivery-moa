bb67d1d270ec58f046ae840a6e090d05a2be6762
LEE TAEWOONG <ltwng99@naver.com>
2025-05-27 16:30:44 +0900

    mypage commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Header from './components/Header'
+import MyPage from './components/pages/myPage/MyPage';
 
 function App() {
 	return (
 		<BrowserRouter basename={import.meta.env.BASE_URL}>
 			<Header />
+			<Link to="/mypage/userinfo">마이페이지</Link>&nbsp;
+			<Link to="/">홈</Link>
+
 			<Routes>
+				<Route path="mypage/*" element={<MyPage />} />
 			</Routes>
 		</BrowserRouter>
 	)
