ab9aad631fe90d93e90022f9924b2f42d0d5c2cb
KimHoSoo217 <huhu0524@naver.com>
2025-05-27 17:52:27 +0900

    Add order complete page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
+import OrderComplete from './components/Order-Complete/OrderComplete'
+import GonguComplete from './components/Gongu-Complete/GonguComplete'
+
 
 function App() {
 	return (
 		<BrowserRouter basename={import.meta.env.BASE_URL}>
 			<Header />
 			<Routes>
+			    <Route path="/ordercomplete" element={<OrderComplete />} /> 
+				<Route path="/gongucomplete" element={<GonguComplete />} /> 
 			</Routes>
 		</BrowserRouter>
 	)
