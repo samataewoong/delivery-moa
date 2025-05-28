0b73b7445f82a6b722d8fc1ea7f8bbbe0583f374
TaYaKi71751 <shantian.tw@gmail.com>
2025-05-28 15:37:56 +0900

    Add header to RoomPage

--- src/pages/room/RoomPage.jsx
+++ src/pages/room/RoomPage.jsx
@@ -2,10 +2,10 @@ import supabase from "../../config/supabaseClient";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import style from "./RoomPage.module.css";
-import Chat from "../../components/chat/Chat";
 import RoomJoin from "../../components/room/RoomJoin";
 import RoomHeader from "../../components/room/RoomHeader";
 import RoomBody from "../../components/room/RoomBody";
+import Header from "../../components/Header";
 
 export default function RoomPage() {
     const { room_id } = useParams();
@@ -22,12 +22,12 @@ export default function RoomPage() {
         fetchUser();
     }, []);
     return (
-
         <div style={{
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
         }}>
+            <Header />
             { uid && <RoomJoin room_id={room_id} user_id={uid} />}
                 <RoomHeader room_id={room_id} />
                 <RoomBody room_id={room_id} />
