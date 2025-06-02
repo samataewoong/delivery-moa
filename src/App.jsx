214caed5eb91a8840dbdec41dc1d44d087fd21af
kahng <gloomy5480@hotmail.com>
2025-06-02 12:07:32 +0900

    주문 내역

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Register from './pages/Register';
-import Header from './components/Header';
-import OrderComplete from './components/Order-Complete/OrderComplete';
-import GonguComplete from './components/Gongu-Complete/GonguComplete';
-import Review from './components/User-Review/Review';
-import Footer from './components/Footer';
-import RoomPage from './pages/room/RoomPage';
-import Login from './pages/Login';
-import MyPage from './pages/myPage/MyPage';
-import UserInfo from './pages/myPage/UserInfo';
-import EditUser from './pages/myPage/EditUser';
-import MyQnA from './pages/myPage/MyQnA';
-import MyReview from './pages/myPage/MyReview';
-import MainPage from './pages/MainPage';
-import RootPage from './pages/RootPage';
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Register from "./pages/Register";
+import Header from "./components/Header";
+import OrderComplete from "./components/Order-Complete/OrderComplete";
+import GonguComplete from "./components/Gongu-Complete/GonguComplete";
+import Review from "./components/User-Review/Review";
+import Footer from "./components/Footer";
+import RoomPage from "./pages/room/RoomPage";
+import Login from "./pages/Login";
+import MyPage from "./pages/myPage/MyPage";
+import UserInfo from "./pages/myPage/UserInfo";
+import EditUser from "./pages/myPage/EditUser";
+import MyQnA from "./pages/myPage/MyQnA";
+import MyReview from "./pages/myPage/MyReview";
+import MainPage from "./pages/MainPage";
+import RootPage from "./pages/RootPage";
+import OrderList from "./pages/MyPage/OrderList";
 
 function App() {
   return (
     <>
       <BrowserRouter basename={import.meta.env.BASE_URL}>
-      
         <Routes>
           <Route path="/" element={<RootPage />} />
           <Route path="/register" element={<Register />} />
@@ -30,8 +29,6 @@ function App() {
           <Route path="/gongucomplete" element={<GonguComplete />} />
           <Route path="/ordercomplete/:order_id" element={<OrderComplete />} />
           <Route path="/review/:room_id" element={<Review />} />
-          
-
           {/* 마이페이지 및 하위 라우트 */}
           <Route path="/mypage" element={<MyPage />}>
             <Route index element={<UserInfo />} />
@@ -39,7 +36,7 @@ function App() {
             <Route path="edituser" element={<EditUser />} />
             <Route path="myqna" element={<MyQnA />} />
             <Route path="myreview" element={<MyReview />} />
-            <Route path="orderlist" element={<div>주문내역 준비중입니다.</div>} />
+            <Route path="orderlist" element={<OrderList />} />
           </Route>
         </Routes>
 
