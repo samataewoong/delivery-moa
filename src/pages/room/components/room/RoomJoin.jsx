4ef714e08c8ed11bc05b4153720ec7e690d9630c
TaYaKi71751 <shantian.tw@gmail.com>
2025-06-02 10:16:21 +0900

    방 삭제시 메인 화면으로 이동

--- src/pages/room/components/room/RoomJoin.jsx
+++ src/pages/room/components/room/RoomJoin.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import InsertRoomJoin from "../../../../functions/room_join/InsertRoomJoin";
 import getAuthUser from "../../../../functions/auth/GetAuthUser";
+import supabase from "../../../../config/supabaseClient";
 
 export default function RoomJoin({ room_id }) {
     const navigate = useNavigate();
@@ -33,7 +34,25 @@ export default function RoomJoin({ room_id }) {
                 navigate("/");
             }
         };
+        const roomSubscribe = supabase
+           .realtime
+           .channel(`realtime:room_status_update_watch_on_room_delete_${room_id}`)
+           .on(
+                "postgres_changes",
+                { event: '*', schema: 'public', table: 'room' },
+                (payload) => {
+                    console.log("Received payload:", payload);
+                    if (payload.new.room_id === Number(room_id) && payload.new.status === "삭제") {
+                        console.log("Room ID matches, navigating to room...");
+                        alert('방이 삭제되었습니다.')
+                        navigate(`/room/${room_id}`);
+                    }
+                }
+            )
         process();
+        return () => {
+            roomSubscribe.unsubscribe();
+        }
     }, [room_id]);
 
     return (
