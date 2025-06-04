3b9de8335d272a16f480c2889095d364a3ea064a
군만두 <won020738@gmail.com>
2025-06-04 16:22:55 +0900

    force

--- src/pages/StoreDetail.jsx
+++ src/pages/StoreDetail.jsx
@@ -26,7 +26,16 @@ export default function StoreDetail(){
         <main className={styles["main_box"]}>
             <div className={styles["main_container"]}>
                 <div><img className={styles["square_img"]} src={`${storeUrl}${store.id}.jpg`}/></div>
-                <div className={styles["square_img"]} >{store.store_name}</div>
+                <div className={styles["detail_box"]}>
+                    <img src="https://epfwvrafnhdgvyfcrhbo.supabase.co/storage/v1/object/public/imgfile/main_img/Vector.png"/>
+                    <div className={styles["detail_text"]}>다같이 주문하면 무료배달</div>
+                    <div className={styles["store_name"]}>{store.store_name}</div>
+                    <div style={{display:"flex", alignItems: "center", gap: "4px" }}>
+                        <div className={styles["star"]}>★</div>
+                        <div className={styles["score"]}>4.9(1689)</div>
+                    </div>
+                    <div className={styles["address_box"]}></div>
+                </div>
             </div>
         </main>
         </>
