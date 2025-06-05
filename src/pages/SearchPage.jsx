394b763c409c015945015a9bb162a27d676e2d33
군만두 <won020738@gmail.com>
2025-06-05 21:52:28 +0900

    검색창

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -9,12 +9,44 @@ export default function SearchPage() {
     const params = new URLSearchParams(location.search);
     const keyword = params.get("keyword");
 
+    useEffect(() => {
+        const fetchResults = async () => {
+            const storeDate = await supabase
+            .from("store").select("*")
+            .ilike("store_name", `%${keyword}%`);
+
+            const menuData = await supabase.from("menu").select(`*, store_id( * )`).ilike("menu_name" , `%${keyword}%`);
+
+            const storeIds = storeDate?.map((s) => s.id) || [];
+
+            const roomData = await supabase.from("room").select("*").ilike("room_name", `%${keyword}%`);
+
+            const roomstoreData = await supabase.from("room").select(`*, store_id( * )`).eq("store_id", storeIds);
+
+            const roomResult = [
+                ...(roomData || []),
+                ...(roomstoreData || []),
+            ].filter(
+                (room, index, self) => self.findIndex((r) => r.id === room.id) === index
+            );
+        };
+        fetchResults();
+    }, [keyword]);
     return(
-        <div className={styles["main_box"]}>
+        <main className={styles["main_box"]}>
             <div className={styles["main_container"]}>
                 <h2>"{keyword}"에 대한 검색결과</h2>
                 <hr/>
+                <div className={styles["search_hitory_box"]}>
+                    <h1>배달</h1>
+                    <div className={styles["search_box"]}>
+
+                    </div>
+                    <hr/>
+                    <h1>공구방</h1>
+                    <hr/>
+                </div>
             </div>
-        </div>
+        </main>
     );
 }
