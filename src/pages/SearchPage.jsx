04df726f817dcf0a3cf89db214d021d2d085e989
군만두 <won020738@gmail.com>
2025-06-09 14:35:30 +0900

    검색창 완성(조건미설정)

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -11,6 +11,7 @@ export default function SearchPage() {
     const [storeData, setStoreData] = useState([]);
     const [menuData, setMenuData] = useState([]);
     const [roomData, setRoomData] = useState([]);
+    const [category, setCategory] = useState("전체");
 
     const storeImgUrl = "https://epfwvrafnhdgvyfcrhbo.supabase.co/storage/v1/object/public/imgfile/store/store_"
     const menuImgUrl = "https://epfwvrafnhdgvyfcrhbo.supabase.co/storage/v1/object/public/imgfile/"
@@ -19,11 +20,11 @@ export default function SearchPage() {
     useEffect(() => {
         const fetchResults = async () => {
             const { data: storeResult } = await supabase
-            .from("store").select("*")
-            .ilike("store_name", `%${keyword}%`);
+                .from("store").select("*")
+                .ilike("store_name", `%${keyword}%`);
             setStoreData(storeResult);
 
-            const { data: menuResult } = await supabase.from("menu").select(`*, store_id( * ), img_id( * )`).ilike("menu_name" , `%${keyword}%`);
+            const { data: menuResult } = await supabase.from("menu").select(`*, store_id( * ), img_id( * )`).ilike("menu_name", `%${keyword}%`);
             setMenuData(menuResult);
 
             const storeIds = storeResult?.map((s) => s.id) || [];
@@ -43,82 +44,102 @@ export default function SearchPage() {
         };
         fetchResults();
     }, [keyword]);
-    return(
+    return (
         <main className={styles["main_box"]}>
             <div className={styles["main_container"]}>
                 <h1>"{keyword}"에 대한 검색결과</h1>
-                <hr/>
+                <hr />
                 <div className={styles["search_hitory_box"]}>
                     <div>
                         <ul className={styles["search_category"]}>
-                            <li>전체</li>
-                            <li>배달</li>
-                            <li>메뉴</li>
-                            <li>공구방</li>
+                            {["전체", "배달", "메뉴", "공구방"].map((tab) => (
+                                <li key={tab} 
+                                className={category === tab ? styles["active_tab"] : ""}
+                                onClick={() => setCategory(tab)}>{tab}</li>
+                            ))}
                         </ul>
                     </div>
-                    <div className={styles["search_keyword"]}>배달</div>
-                    <hr/>
-                        {storeData.length > 0 ? (
-                            storeData.slice(0, 4).map((item)=>(
-                                <div className={styles["search_result"]}>
-                                    <img className={styles["search_store_img"]} src={`${storeImgUrl}${item.id}.jpg`} alt={`${item.store_name}`}></img>
-                                    <div className={styles["search_store_detail"]}>
-                                        <div>
-                                            <div className={styles["search_store_name"]}>{item.store_name}</div>
-                                            <ul className={styles["search_ul"]}>
-                                                <li>
-                                                    <span className={styles["star"]}>★</span>
-                                                    <span className={styles["score"]}>4.9(1689)</span>
-                                                </li>
-                                                <li>210m</li>
-                                                <li>25~40분</li>
-                                            </ul>
-                                            <div className={styles["search_delivery"]}>배달비 무료</div>
+                    {(category === "전체" || category === "배달") && (
+                        <>
+                            <div className={styles["search_keyword"]}>배달</div>
+                            <hr />
+                            {storeData.length > 0 ? (
+                                (category === "전체" ? storeData.slice(0, 4) : storeData).map((item) => (
+                                    <div className={styles["search_result"]}>
+                                        <img className={styles["search_store_img"]} src={`${storeImgUrl}${item.id}.jpg`} alt={`${item.store_name}`}></img>
+                                        <div className={styles["search_store_detail"]}>
+                                            <div>
+                                                <div className={styles["search_store_name"]}>{item.store_name}</div>
+                                                <ul className={styles["search_ul"]}>
+                                                    <li>
+                                                        <span className={styles["star"]}>★</span>
+                                                        <span className={styles["score"]}>4.9(1689)</span>
+                                                    </li>
+                                                    <li>210m</li>
+                                                    <li>25~40분</li>
+                                                </ul>
+                                                <div className={styles["search_delivery"]}>배달비 무료</div>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            ))
-                        ) : (
-                            <div className={styles["search_noResult"]}>검색 결과가 없습니다.</div>
-                        )}
-                    <div className={styles["search_keyword"]}>메뉴</div>
-                    <hr/>
-                        {menuData.length > 0 ? (
-                            menuData.slice(0, 4).map((item)=>(
-                                <div className={styles["search_result"]}>
-                                    <img className={styles["search_store_img"]} src={`${menuImgUrl}${item.img_id?.folder}/${item.img_id?.filename}`} alt={`${item.menu_name}`}></img>
-                                    <div className={styles["search_store_detail"]}>
-                                        <div>
-                                            <div className={styles["search_store_name"]}>{item.menu_name}</div>
-                                            <div className={styles["search_menu_name"]}>{item.store_id?.store_name}</div>
-                                            <div className={styles["search_menu_price"]}>{item.menu_price}원</div>
+                                ))
+                            ) : (
+                                <div className={styles["search_noResult"]}>검색 결과가 없습니다.</div>
+                            )}
+                        </>
+                    )}
+                    {(category === "전체" || category === "메뉴") && (
+                        <>
+                            <div className={styles["search_keyword"]}>메뉴</div>
+                            <hr />
+                            {menuData.length > 0 ? (
+                                (category === "전체" ? menuData.slice(0, 4) : menuData).map((item) => (
+                                    <div className={styles["search_result"]}>
+                                        <img className={styles["search_store_img"]} src={`${menuImgUrl}${item.img_id?.folder}/${item.img_id?.filename}`} alt={`${item.menu_name}`}></img>
+                                        <div className={styles["search_store_detail"]}>
+                                            <div>
+                                                <div className={styles["search_store_name"]}>{item.menu_name}</div>
+                                                <div className={styles["search_menu_name"]}>{item.store_id?.store_name}</div>
+                                                <div className={styles["search_menu_price"]}>{item.menu_price}원</div>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            ))
-                        ) : (
-                            <div className={styles["search_noResult"]}>검색 결과가 없습니다.</div>
-                        )}
-                    <div className={styles["search_keyword"]}>공구방</div>
-                    <hr/>
-                        {roomData.length > 0 ? (
-                            roomData.slice(0, 4).map((item)=>(
-                                <div className={styles["search_result"]}>
-                                    <img className={styles["search_store_img"]} 
-                                    src={`${storeImgUrl}${item.store_id}.jpg`} alt={`${item.store_id}`}></img>
-                                    <div className={styles["search_store_detail"]}>
-                                        <div>
-                                        {item.room_name}
-                                        {item.room_address}
-                                        {item.room_status}
+                                ))
+                            ) : (
+                                <div className={styles["search_noResult"]}>검색 결과가 없습니다.</div>
+                            )}
+                            
+                        </>
+                    )}
+                    {(category === "전체" || category === "공구방") && (
+                        <>
+                            <div className={styles["search_keyword"]}>공구방</div>
+                            <hr />
+                            {roomData.length > 0 ? (
+                                (category === "전체" ? roomData.slice(0, 4) : roomData).map((item) => (
+                                    <div className={styles["search_result"]}>
+                                        <img className={styles["search_store_img"]}
+                                            src={`${storeImgUrl}${item.store_id}.jpg`} alt={`${item.store_id}`}></img>
+                                        <div className={styles["search_store_detail"]}>
+                                            <div>
+                                                <div className={styles["search_store_name"]}>{item.room_name}</div>
+                                                <div className={styles["search_menu_name"]}>{item.room_address}</div>
+                                                <div className={styles["search_status"]}>{item.status}</div>
+                                            </div>
                                         </div>
                                     </div>
+                                ))
+                            ) : (
+                                <div className={styles["search_noResult"]}>검색 결과가 없습니다.</div>
+                            )}
+                            {category === "전체" && roomData.length > 4 && (
+                                <div className={styles["more"]}>
+                                    <button onClick={() => {setCategory("공구방");
+                                    window.scrollTo({ top: 0 });}}>더보기</button>
                                 </div>
-                            ))
-                        ) : (
-                            <div className={styles["search_noResult"]}>검색 결과가 없습니다.</div>
-                        )}
+                            )}
+                        </>
+                    )}
                 </div>
             </div>
         </main>
