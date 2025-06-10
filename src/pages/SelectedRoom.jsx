507f0f1702a620eecc709b6707b53dfd8bbe2cca
군만두 <won020738@gmail.com>
2025-06-10 15:18:28 +0900

    개설된 방 완성

---
+++ src/pages/SelectedRoom.jsx
@@ -0,0 +1,60 @@
+import styles from "./SelectedRoom.module.css";
+import { useState, useEffect, useRef } from "react";
+import supabase from "../config/supabaseClient";
+import { Link, useNavigate } from "react-router-dom";
+import { useParams } from 'react-router-dom';
+import thousands from "thousands";
+
+export default function SelectRoom() {
+    const { store_id } = useParams();
+    const [ roomSelect, setRoomSelect ] = useState([]);
+    const [ store, setStore ] = useState([]);
+
+    useEffect(() => {
+        const fetchResults = async () => {
+            const { data: roomResultData } = await supabase.from("room").select("*").eq("store_id", store_id);
+            setRoomSelect(roomResultData);
+
+            const { data } = await supabase.from("store").select("*").eq("id",store_id).single();
+            setStore(data);
+        }
+        fetchResults();
+    }, [store_id]);
+
+    const storeImgUrl = "https://epfwvrafnhdgvyfcrhbo.supabase.co/storage/v1/object/public/imgfile/store/store_"
+
+    return (
+        <main className={styles["main_container"]}>
+            <div className={styles["main_contents"]}>
+                <div className={styles["main_head"]}>
+                    <img onClick={() => {
+                    navigate(-1);
+                }} className={styles["back_btn"]} src="https://epfwvrafnhdgvyfcrhbo.supabase.co/storage/v1/object/public/imgfile/main_img/backbtn.png"></img>
+                    <div>개설된방 : </div>
+                    <div className={styles["store_name"]}>&nbsp;{store.store_name}</div>
+                </div>
+                <div className={styles["main_box"]}>
+                    {roomSelect.length > 0 ? (
+                        roomSelect.map((item) => (
+                            <Link key={item.id} to={`/room/${item.id}`}>
+                                <div className={styles["search_result"]}>
+                                    <img className={styles["search_store_img"]}
+                                        src={`${storeImgUrl}${item.store_id}.jpg`} alt={`${item.store_id}`}></img>
+                                    <div className={styles["search_store_detail"]}>
+                                        <div>
+                                            <div className={styles["search_store_name"]}>{item.room_name}</div>
+                                            <div className={styles["search_menu_name"]}>{item.room_address}</div>
+                                            <div className={styles["search_status"]}>{item.status}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Link>
+                        ))
+                    ) : (
+                        <div className={styles["search_noResult"]}>검색 결과가 없습니다.</div>
+                    )}
+                </div>
+            </div>
+        </main>
+    )
+}
