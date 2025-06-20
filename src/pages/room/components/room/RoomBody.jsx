1f34889a6e94c45e24716e7c46c86f072b4be02e
TaYaKi71751 <shantian.tw@gmail.com>
2025-06-21 08:37:18 +0900

    realtime 추가 및 오류 수정

--- src/pages/room/components/room/RoomBody.jsx
+++ src/pages/room/components/room/RoomBody.jsx
@@ -18,14 +18,14 @@ export default function RoomBody({
         <div className={style.room_body}>
             <div className={style.room_body_left}>
                 <div className={style.room_body_left_top}>
-                    <RoomOrderStatus room={room} roomJoin={roomJoin} me={me} />
+                    <RoomOrderStatus room={room} room_id={room_id} roomJoin={roomJoin} me={me} />
                 </div>
                 <div className={style.room_body_left_bottom}>
                     <Chat room_id={room_id} />
                 </div>
             </div>
             <div className={style.room_body_right}>
-                <RoomMenu room={room} setRoomMenus={setRoomMenus} store={store} roomMenus={roomMenus} me={me} />
+                <RoomMenu room={room} room_id={room_id} setRoomMenus={setRoomMenus} store={store} roomMenus={roomMenus} me={me} />
             </div>
         </div>
     );
