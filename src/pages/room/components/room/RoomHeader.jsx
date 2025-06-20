e305859166c1913324fbca43b3f09cac44562bdc
TaYaKi71751 <shantian.tw@gmail.com>
2025-06-20 20:25:02 +0900

    오타 수정

--- src/pages/room/components/room/RoomHeader.jsx
+++ src/pages/room/components/room/RoomHeader.jsx
@@ -21,6 +21,9 @@ export default function RoomHeader({
 
     useEffect(() => {
         async function fetchCanLeave() {
+            if (!me) {
+                return;
+            }
             if (
                 room
                 && roomJoin
